632ec42cb9a8d2f37c0171ba1b356add7d55aced
romulonishiyama <romulonishiyama@gmail.com>
2023-08-11 10:25:34 -0300

    3

--- pages/japones.vue
+++ pages/japones.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1 class="text-white">Japonês</h1>
+    <h2 class="text-white" :class="cor">Cor: {{ cor }}</h2>
+    <button @click="mudarCor('vermelho')">Mudar cor para vermelho</button>
+    <button @click="mudarCor('azul')">Mudar cor para azul</button>
 </template>
+
+<script lang="ts" setup>
+
+const cor = ref('');
+
+const mudarCor = (c: string) => {
+    console.log(c)
+    cor.value = c;
+}
+</script>
+
+<style scoped>
+.azul {
+    background-color: blue;
+}
+
+.vermelho {
+    background-color: red;
+}
+</style>
