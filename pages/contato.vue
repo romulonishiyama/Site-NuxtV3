9a6e96f886d9cbb31c3bdc585ed6185105ed5c36
romulo nishiyama <romulonishiyama@gmail.com>
2023-08-14 21:54:32 -0300

    3

--- pages/contato.vue
+++ pages/contato.vue
@@ -11,29 +11,32 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="us in users">
-                    <td> {{ users.indexOf(us) }} </td>
+                <tr v-for="(us, index) in users">
+                    <td> {{ index + 1 }} </td>
                     <td>{{ us.first_name }}</td>
-                    <td><button class="btn btn-primary">Editar</button><button class="btn btn-danger" :id=users.indexOf(us)  @click="removerContato($event)">Deletar</button></td>
+                    <td>
+                        <button class="btn btn-primary">Editar</button>
+                        <button class="btn btn-danger" :id="index" @click="removerContato($event)">Deletar</button>
+                    </td>
+
                 </tr>
-                {{ users }}
             </tbody>
         </table>
-
     </div>
 </template>
 <script lang="ts" setup>
 
+let u = ref(0)
 const users = computed(() => {
-    let u = useUsuarios().usuarios;
-    return u
+    u.value = useUsuarios().usuarios;
+    console.log(u.value)
+    return u.value
 })
-
 
 const removerContato = (e) => {
     let posicao = e.target.id
-    users.value.splice((posicao - 1), 1)
-    console.log(users)
+    u.value.splice(posicao, 1)
+    console.log(posicao)
 
 }
 
